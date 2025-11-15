3bd2123cdf6becad3d004b8568aaa725edeea578
AKA: NeO <103208363+aadityabhoyarr@users.noreply.github.com>
2025-11-16 00:33:04 +0530

    Display player window
    Updated main.java

--- main.java
+++ main.java
@@ -2,17 +2,28 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.image.*;
 
+public class Main extends Application {
 
-public class Main extends Application{
+    public void start(Stage stage) {
 
-	public void start (Stage stage){
-		stage.setScene(new Scene(new Pane(), 800, 600));  // show screen
-    		stage.show();
-	}
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane, 960, 540);
 
+        Image img = new Image("file:Assets/player.png"); // load pic
+        ImageView player = new ImageView(img); // show pic
 
-	public static void main(String[] argh){
-		launch();
-	}
+        player.setLayoutX(100); // player x-pos
+        player.setLayoutY(350); // player y-pos
+
+        pane.getChildren().add(player);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
