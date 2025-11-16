8be2982a1b052819f3d8f1ee040ccde4343a8364
Ankit-Dubey04 <ankitd0224@gmail.com>
2025-11-16 21:00:56 +0530

    updated map

--- main.java
+++ main.java
@@ -1,4 +1,4 @@
-//import javax.swing.text.html.ImageView; //wtf is this swing??? per q 😭
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -8,36 +8,41 @@ import javafx.scene.input.*;
 
 public class Main extends Application {
     //tiles
-    int TILE_SIZE = 40;
+     final int TILE_SIZE = 40;
 
     //map using tile
     int[][] map = {
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {1,1,1,1,1,1,1,1,1,1},
-        {1,1,1,1,1,1,1,1,1,1}
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
     };
 
     public float x = 100;
     public float y = 350;
+     
 
     ImageView player;
-
+    Pane pane = new Pane();
+    Image tileimage = new Image("file:Assets/tile.png");
     public void start(Stage stage) {
 
-        Pane pane = new Pane();
+        
         Scene scene = new Scene(pane, 960, 540);
 
-        // loadimage();  // not working bro
+        loadmap();  
 
-        // Image tileimage = new tileimage("file:Assets/player.png"); // not working bro
-
+        
         Image img = new Image("file:Assets/player.png"); // load pic
-        player = new ImageView(img); // show pic
+        player = new ImageView(img); 
 
         player.setLayoutX(x); // player x-pos
         player.setLayoutY(y); // player y-pos
@@ -50,18 +55,20 @@ public class Main extends Application {
         stage.show();	
     }
 
-    /*
-    public void loadimage(){
+    
+    public void loadmap(){
         for(int row=0; row < map.length; row++){
             for(int col = 0; col < map[row].length; col++){
-                ImageView tile = new ImageView(tileimage);
-                tile.setLayoutX(col * TILE_SIZE);
-                tile.setLayoutY(row * TILE_SIZE);
-                pane.getChildren().add(tile);
+                if(map[row][col]==1){
+                    ImageView tile = new ImageView(tileimage);
+                    tile.setLayoutX(col * TILE_SIZE);
+                    tile.setLayoutY(row * TILE_SIZE);
+                    pane.getChildren().add(tile);
+                } 
             }
         }
     }
-    */
+    
 
      public void movement(Scene scene){
 
