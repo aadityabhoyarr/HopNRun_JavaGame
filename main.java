69c499206e4ca141d26f4ac01f2ae7029c4cc17c
AKA: NeO <103208363+aadityabhoyarr@users.noreply.github.com>
2025-11-29 22:06:54 +0530

    Update

--- main.java
+++ main.java
@@ -6,388 +6,320 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import javafx.scene.text.Text;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import java.util.ArrayList;
 import java.util.Random;
 
-// this is parent class for all game things
+
+// ------------------------ BASE CLASS (player, ground tile) ------------------------
 class GameObject {
     ImageView image;
-    int posX;
-    int posY;
-    int width;
-    int height;
+    int x, y, w, h;
 
-    // constructor to make game object
-    public GameObject(String imagePath, int startX, int startY, int objWidth, int objHeight) {
-        image = new ImageView(new Image(imagePath));
-        image.setFitWidth(objWidth);
-        image.setFitHeight(objHeight);
-        posX = startX;
-        posY = startY;
-        width = objWidth;
-        height = objHeight;
-        image.setLayoutX(posX);
-        image.setLayoutY(posY);
+    public GameObject(String img, int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+
+        image = new ImageView(new Image(img));
+        image.setFitWidth(w);
+        image.setFitHeight(h);
+        image.setLayoutX(x);
+        image.setLayoutY(y);
     }
 
-    // move object left or right
-    public void moveObject(int speed) {
-        posX = posX + speed;
-        image.setLayoutX(posX);
+    void moveX(int speed) {
+        x += speed;
+        image.setLayoutX(x);
     }
 
-    // get the image
-    public ImageView getImage() {
-        return image;
+    void setX(int newX) {
+        x = newX;
+        image.setLayoutX(x);
     }
 
-    // get x position
-    public int getXPosition() {
-        return posX;
-    }
-
-    // set x position
-    public void setXPosition(int newX) {
-        posX = newX;
-        image.setLayoutX(posX);
-    }
-
-    // set y position
-    public void setYPosition(int newY) {
-        posY = newY;
-        image.setLayoutY(posY);
+    void setY(int newY) {
+        y = newY;
+        image.setLayoutY(y);
     }
 }
 
-// this is the player class
-class Player extends GameObject {
-    boolean isJumping;
-    double jumpSpeed;
-    double fallSpeed;
 
-    // make player
-    public Player(String imagePath, int startX, int startY) {
-        super(imagePath, startX, startY, 40, 40);
-        isJumping = false;
-        jumpSpeed = 0;
-        fallSpeed = 0.25;
+// ------------------------ PLAYER ------------------------
+class Player extends GameObject {
+    boolean jumping = false;
+    double jumpSpeed = 0;
+    double gravity = 0.25;
+
+    public Player(String img, int x, int y) {
+        super(img, x, y, 40, 40);
     }
 
-    // make player jump
-    public void jump() {
-        if (isJumping == false) {
-            isJumping = true;
+    void jump() {
+        if (!jumping) {
+            jumping = true;
             jumpSpeed = -9;
-            fallSpeed = 0.15;
+            gravity = 0.15;
         }
     }
 
-    // update player position when jumping
-    public void updatePosition(int groundLevel) {
-        if (isJumping == true) {
-            jumpSpeed = jumpSpeed + fallSpeed;
-            posY = (int)(posY + jumpSpeed);
+    void update(int groundY) {
+        if (jumping) {
+            jumpSpeed += gravity;
+            y += jumpSpeed;
 
-            // check if player hit ground
-            if (posY >= groundLevel - 40) {
-                posY = groundLevel - 40;
-                isJumping = false;
+            if (y >= groundY - h) {
+                y = groundY - h;
+                jumping = false;
                 jumpSpeed = 0;
-                fallSpeed = 0.25;
+                gravity = 0.25;
             }
 
-            image.setLayoutY(posY);
+            image.setLayoutY(y);
         }
     }
 
-    // check if jumping
-    public boolean checkJumping() {
-        return isJumping;
-    }
-
-    // stop jump
-    public void stopJumping() {
-        isJumping = false;
+    void resetJump() {
+        jumping = false;
         jumpSpeed = 0;
-        fallSpeed = 0.25;
+        gravity = 0.25;
     }
 }
 
-// this is ground tile class
+
+// ------------------------ GROUND TILE ------------------------
 class GroundTile extends GameObject {
-    // make ground tile
-    public GroundTile(String imagePath, int startX, int startY, int tileSize) {
-        super(imagePath, startX, startY, tileSize, tileSize);
+    public GroundTile(String img, int x, int y, int size) {
+        super(img, x, y, size, size);
     }
 
-    // scroll ground tile
-    public void scrollTile(int scrollSpeed, int maxWidth, int totalTiles, int tileSize) {
-        moveObject(scrollSpeed);
-        // if tile goes off screen move it back
-        if (posX < -tileSize) {
-            posX = posX + (tileSize * totalTiles);
-            image.setLayoutX(posX);
+    void scroll(int speed, int totalTiles, int tileSize) {
+        moveX(speed);
+
+        if (x < -tileSize) {
+            x += tileSize * totalTiles;
+            image.setLayoutX(x);
         }
     }
 }
 
-// this is obstacle class
+
+// ------------------------ OBSTACLE ------------------------
 class Obstacle {
-    Pane obstacleBox;
-    int obstacleX;
-    int obstacleY;
-    int obstacleHeight;
-    int blockSize;
+    Pane box = new Pane();
+    int x, y, size, height;
 
-    // make obstacle
-    public Obstacle(String imagePath, int startX, int startY, int size) {
-        obstacleBox = new Pane();
-        obstacleX = startX;
-        obstacleY = startY;
-        blockSize = size;
-        Random rand = new Random();
-        obstacleHeight = rand.nextInt(2) + 1;
+    public Obstacle(String img, int x, int y, int size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
 
-        createObstacle(imagePath);
-        obstacleBox.setLayoutX(obstacleX);
-        obstacleBox.setLayoutY(obstacleY);
+        resetHeight(img);
+        box.setLayoutX(x);
+        box.setLayoutY(y);
     }
 
-    // create obstacle blocks
-    public void createObstacle(String imagePath) {
-        obstacleBox.getChildren().clear();
-        for (int i = 0; i < obstacleHeight; i++) {
-            ImageView blockPart = new ImageView(new Image(imagePath));
-            blockPart.setFitWidth(blockSize);
-            blockPart.setFitHeight(blockSize);
-            blockPart.setLayoutY(-i * blockSize);
-            obstacleBox.getChildren().add(blockPart);
+    void resetHeight(String img) {
+        height = new Random().nextInt(2) + 1; // 1 or 2 blocks
+        box.getChildren().clear();
+
+        for (int i = 0; i < height; i++) {
+            ImageView block = new ImageView(new Image(img));
+            block.setFitWidth(size);
+            block.setFitHeight(size);
+            block.setLayoutY(-i * size);
+            box.getChildren().add(block);
         }
     }
 
-    // move obstacle
-    public void moveObstacle(int speed) {
-        obstacleX = obstacleX + speed;
-        obstacleBox.setLayoutX(obstacleX);
+    void move(int speed) {
+        x += speed;
+        box.setLayoutX(x);
     }
 
-    // reset obstacle position
-    public void resetObstacle(String imagePath, int windowWidth) {
-        Random rand = new Random();
-        obstacleHeight = rand.nextInt(2) + 1;
-        createObstacle(imagePath);
-        int randomGap = 120 + rand.nextInt(200);
-        obstacleX = windowWidth + randomGap;
-        obstacleBox.setLayoutX(obstacleX);
-    }
-
-    // get obstacle box
-    public Pane getObstacleBox() {
-        return obstacleBox;
-    }
-
-    // get x position
-    public int getObstacleX() {
-        return obstacleX;
+    void reset(String img, int screenWidth) {
+        resetHeight(img);
+        x = screenWidth + (120 + new Random().nextInt(200));
+        box.setLayoutX(x);
     }
 }
 
-// main game class
+
+// ------------------------ MAIN GAME ------------------------
 public class Main extends Application {
 
-    // window size
-    int windowWidth = 800;
-    int windowHeight = 400;
-    int tileSize = 40;
+    int WIDTH = 800;
+    int HEIGHT = 400;
+    int TILE = 40;
+    int groundY;
 
-    Pane gameScreen = new Pane();
+    Pane screen = new Pane();
+    Scene scene;   // <-- FIXED (now global variable)
 
-    // game objects
     Player player;
     Obstacle obstacle;
-    ArrayList<GroundTile> groundTiles = new ArrayList<>();
+    ArrayList<GroundTile> ground = new ArrayList<>();
 
-    // game state
-    boolean isGameOver = false;
+    boolean gameOver = false;
 
-    // score variables
     int score = 0;
     Text scoreText;
-    ImageView coinImage;
+    ImageView coinIcon;
 
-    // game over stuff
     Text gameOverText;
-    Button restartButton;
+    Button restartBtn;
 
-    int groundLevel;
+    Font smallFont, bigFont, buttonFont;
 
-    // fonts
-    Font smallFont;
-    Font bigFont;
-    Font buttonFont;
 
     @Override
     public void start(Stage stage) {
 
-        // load fonts
+        // fonts
         smallFont = Font.loadFont("file:Assets/pixle.ttf", 24);
         bigFont = Font.loadFont("file:Assets/pixle.ttf", 40);
         buttonFont = Font.loadFont("file:Assets/pixle.ttf", 22);
 
-        // create game scene
-        Scene gameScene = new Scene(gameScreen, windowWidth, windowHeight, Color.SKYBLUE);
+        // scene  (stored globally)
+        scene = new Scene(screen, WIDTH, HEIGHT, Color.SKYBLUE);
 
-        // setup game elements
+        // create game objects
         createGround();
         createPlayer();
         createObstacle();
-        createScoreDisplay();
+        createScore();
 
-        // handle spacebar for jump
-        gameScene.setOnKeyPressed(event -> {
-            if (event.getCode().toString().equals("SPACE") && isGameOver == false) {
+        // jump
+        scene.setOnKeyPressed(e -> {
+            if (e.getCode().toString().equals("SPACE") && !gameOver) {
                 player.jump();
             }
         });
 
-        // game loop - runs every frame
+        // game loop
         new AnimationTimer() {
             @Override
             public void handle(long now) {
 
-                // check if player hit obstacle
-                if (isGameOver == false && player.getImage().getBoundsInParent().intersects(obstacle.getObstacleBox().getBoundsInParent())) {
-                    isGameOver = true;
+                // 1. collision
+                if (!gameOver &&
+                    player.image.getBoundsInParent().intersects(
+                    obstacle.box.getBoundsInParent())) {
+                    gameOver = true;
                     showGameOver();
                 }
 
-                // stop game if dead
-                if (isGameOver == true) {
-                    return;
-                }
+                if (gameOver) return;
 
-                // update game
-                player.updatePosition(groundLevel);
+                // 2. update
+                player.update(groundY);
                 updateGround();
                 updateObstacle();
 
-                // increase score
-                score = score + 1;
+                // 3. score
+                score++;
                 scoreText.setText(String.valueOf(score));
             }
         }.start();
 
-        stage.setScene(gameScene);
+        stage.setScene(scene);
         stage.setTitle("HopNRun");
         stage.show();
     }
 
-    // create ground tiles
-    private void createGround() {
-        int numberOfTiles = windowWidth / tileSize;
-        groundLevel = windowHeight - tileSize;
 
-        for (int i = 0; i < numberOfTiles + 2; i++) {
-            GroundTile tile = new GroundTile("file:Assets/tile.png", i * tileSize, groundLevel, tileSize);
-            groundTiles.add(tile);
-            gameScreen.getChildren().add(tile.getImage());
+    // ------------------------ CREATE ELEMENTS ------------------------
+
+    void createGround() {
+        groundY = HEIGHT - TILE;
+        int tilesNeeded = WIDTH / TILE + 2;
+
+        for (int i = 0; i < tilesNeeded; i++) {
+            GroundTile t = new GroundTile("file:Assets/tile.png", i * TILE, groundY, TILE);
+            ground.add(t);
+            screen.getChildren().add(t.image);
         }
     }
 
-    // update ground scrolling
-    private void updateGround() {
-        for (GroundTile tile : groundTiles) {
-            tile.scrollTile(-2, -tileSize, groundTiles.size(), tileSize);
-        }
+    void updateGround() {
+        for (GroundTile t : ground)
+            t.scroll(-2, ground.size(), TILE);
     }
 
-    // create player
-    private void createPlayer() {
-        player = new Player("file:Assets/player.png", 100, windowHeight - tileSize - 40);
-        gameScreen.getChildren().add(player.getImage());
+    void createPlayer() {
+        player = new Player("file:Assets/player.png", 100, groundY - 40);
+        screen.getChildren().add(player.image);
     }
 
-    // create obstacle
-    private void createObstacle() {
-        obstacle = new Obstacle("file:Assets/tile.png", windowWidth + 150, groundLevel - 40, tileSize);
-        gameScreen.getChildren().add(obstacle.getObstacleBox());
+    void createObstacle() {
+        obstacle = new Obstacle("file:Assets/tile.png", WIDTH + 150, groundY - 40, TILE);
+        screen.getChildren().add(obstacle.box);
     }
 
-    // update obstacle movement
-    private void updateObstacle() {
-        obstacle.moveObstacle(-2);
+    void updateObstacle() {
+        obstacle.move(-2);
 
-        // if obstacle goes off screen reset it
-        if (obstacle.getObstacleX() < -tileSize) {
-            obstacle.resetObstacle("file:Assets/tile.png", windowWidth);
-        }
+        if (obstacle.x < -TILE)
+            obstacle.reset("file:Assets/tile.png", WIDTH);
     }
 
-    // create score display
-    private void createScoreDisplay() {
-        // coin icon
-        coinImage = new ImageView(new Image("file:Assets/coin.png"));
-        coinImage.setFitWidth(30);
-        coinImage.setFitHeight(30);
-        coinImage.setLayoutX(windowWidth - 130);
-        coinImage.setLayoutY(20);
+    void createScore() {
+        coinIcon = new ImageView(new Image("file:Assets/coin.png"));
+        coinIcon.setFitWidth(30);
+        coinIcon.setFitHeight(30);
+        coinIcon.setLayoutX(WIDTH - 130);
+        coinIcon.setLayoutY(20);
 
-        // score text
         scoreText = new Text("0");
         scoreText.setFont(smallFont);
         scoreText.setFill(Color.BLACK);
-        scoreText.setLayoutX(windowWidth - 90);
+        scoreText.setLayoutX(WIDTH - 90);
         scoreText.setLayoutY(42);
 
-        gameScreen.getChildren().addAll(coinImage, scoreText);
+        screen.getChildren().addAll(coinIcon, scoreText);
     }
 
-    // show game over screen
-    private void showGameOver() {
-        // game over text
+
+    // ------------------------ GAME OVER ------------------------
+
+    void showGameOver() {
         gameOverText = new Text("GAME OVER");
         gameOverText.setFont(bigFont);
         gameOverText.setFill(Color.RED);
-        gameOverText.setLayoutX(windowWidth / 2 - 140);
-        gameOverText.setLayoutY(windowHeight / 2 - 20);
+        gameOverText.setLayoutX(WIDTH / 2 - 140);
+        gameOverText.setLayoutY(HEIGHT / 2 - 20);
 
-        // restart button
-        restartButton = new Button("RESTART");
-        restartButton.setFont(buttonFont);
-        restartButton.setLayoutX(windowWidth / 2 - 60);
-        restartButton.setLayoutY(windowHeight / 2 + 20);
-        restartButton.setPrefWidth(120);
-        restartButton.setOnAction(event -> restartGame());
+        restartBtn = new Button("RESTART");
+        restartBtn.setFont(buttonFont);
+        restartBtn.setLayoutX(WIDTH / 2 - 60);
+        restartBtn.setLayoutY(HEIGHT / 2 + 20);
+        restartBtn.setPrefWidth(120);
+        restartBtn.setOnAction(e -> restartGame());
 
-        gameScreen.getChildren().addAll(gameOverText, restartButton);
+        screen.getChildren().addAll(gameOverText, restartBtn);
     }
 
-    // restart game
-    private void restartGame() {
-        // reset game state
-        isGameOver = false;
-        player.stopJumping();
-        player.setXPosition(100);
-        player.setYPosition(groundLevel - 40);
+    void restartGame() {
+        gameOver = false;
 
-        // reset obstacle
-        obstacle.resetObstacle("file:Assets/tile.png", windowWidth);
-        obstacle.getObstacleBox().setLayoutY(groundLevel - 40);
+        player.resetJump();
+        player.setX(100);
+        player.setY(groundY - 40);
 
-        // reset score
+        obstacle.reset("file:Assets/tile.png", WIDTH);
+        obstacle.box.setLayoutY(groundY - 40);
+
         score = 0;
         scoreText.setText("0");
 
-        // remove game over elements
-        gameScreen.getChildren().remove(gameOverText);
-        gameScreen.getChildren().remove(restartButton);
+        screen.getChildren().removeAll(gameOverText, restartBtn);
 
-        // fix background color
-        gameScreen.setStyle("-fx-background-color: skyblue;");
+        screen.setStyle("-fx-background-color: skyblue;"); // this is because whne the game resart it should be blue again
     }
+
 
     public static void main(String[] args) {
         launch();
